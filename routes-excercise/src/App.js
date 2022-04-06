d24c065c139369c8d3e6af1ceab4c0097d02422c
Aashar ck <aasharck@gmail.com>
2022-04-06 18:07:27 +0530

    added routes-exc

---
+++ routes-excercise/src/App.js
@@ -0,0 +1,51 @@
+import { React, Component } from 'react';
+import './App.css';
+import Navbar from './Navbar';
+
+class App extends Component{
+  static defaultProps = {
+    dogs: [
+      {
+        name: "Whiskey",
+        age: 5,
+        src: whiskey,
+        facts: [
+          "Whiskey loves eating popcorn.",
+          "Whiskey is a terrible guard dog.",
+          "Whiskey wants to cuddle with you!"
+        ]
+      },
+      {
+        name: "Hazel",
+        age: 3,
+        src: hazel,
+        facts: [
+          "Hazel has soooo much energy!",
+          "Hazel is highly intelligent.",
+          "Hazel loves people more than dogs."
+        ]
+      },
+      {
+        name: "Tubby",
+        age: 4,
+        src: tubby,
+        facts: [
+          "Tubby is not the brightest dog",
+          "Tubby does not like walks or exercise.",
+          "Tubby loves eating food."
+        ]
+      }
+    ]
+  }
+  render(){
+    return (
+      <div className="App">
+        <Navbar />
+        <h1 className='display-1'>Hello</h1>
+        </div>
+    );
+  }
+  
+}
+
+export default App;
